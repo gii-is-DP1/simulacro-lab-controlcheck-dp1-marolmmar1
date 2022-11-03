e170528470c2ccd69c1dc2fa57035aa667f4c7aa
Usuario <Usuario@MSI>
2022-11-03 10:39:41 +0100

    entrega

--- src/main/java/org/springframework/samples/petclinic/product/ProductRepository.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductRepository.java
@@ -3,14 +3,27 @@ package org.springframework.samples.petclinic.product;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 
 
-public interface ProductRepository {
-    List<Product> findAll();
+public interface ProductRepository extends CrudRepository<Product, Integer>{
+	
+	@Query("SELECT p FROM ProductType p")
     List<ProductType> findAllProductTypes();
+	
+	@Query("SELECT p FROM Product p")
+	List<Product> findAll();   
+    
     Optional<Product> findById(int id);
+    
+    @Query("SELECT p FROM ProductType p WHERE p.name = :name")
+    ProductType findProductTypeByName(String name);
+    
     Product findByName(String name);
-    Product save(Product p);
+    
+    @Query("SELECT p FROM Product p WHERE p.price < :price")
+    List<Product> findByPriceLessThan(double price);
+    
 }
--- src/main/java/org/springframework/samples/petclinic/product/ProductTypeFormatter.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductTypeFormatter.java
@@ -1,24 +1,38 @@
 package org.springframework.samples.petclinic.product;
 
 import java.text.ParseException;
+import java.util.Collection;
 import java.util.Locale;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.Formatter;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProductTypeFormatter implements Formatter<ProductType>{
-
+	
+	private final ProductService prodService;
+	
+	@Autowired
+	public ProductTypeFormatter(ProductService prodService) {
+		this.prodService = prodService;
+	}
+	
     @Override
-    public String print(ProductType object, Locale locale) {
+    public String print(ProductType prodType, Locale locale) {
         // TODO Auto-generated method stub
-        return null;
+        return prodType.getName();
     }
 
     @Override
     public ProductType parse(String text, Locale locale) throws ParseException {
-        // TODO Auto-generated method stub
-        return null;
+        Collection<ProductType> findProductTypes = this.prodService.getAllProductTypes();
+        for(ProductType type : findProductTypes) {
+        	if(type.getName().equals(text)) {
+        		return type;
+        	}
+        }
+        throw new ParseException("type not found: " + text, 0);
     }
     
 }
